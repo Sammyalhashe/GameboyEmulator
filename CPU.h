6acc517eb0292d5447c2c9bc32a7bf4ca4d3ac8a
Sammyalhashe <sammy.alhashemi@mail.utoronto.ca>
2020-04-12 20:12:52 -0400

    Adding definitions for SUB, SUBC files. Forgot to push

--- CPU.h
+++ CPU.h
@@ -200,6 +200,9 @@ private: //OPCODES
     int ADD_A_Addr_REG16(uint16_t REG);
     int ADC_A_REG(uint8_t REG);
     int ADC_A_Addr_REG16(uint16_t REG);
+    int SUB_A_REG(uint8_t REG);
+    int SUB_A_Addr_REG16(uint16_t REG);
+    int SBC_A_REG(uint8_t REG);
 
     /** Helper functions to read memory **/
     uint16_t ReadNn();
